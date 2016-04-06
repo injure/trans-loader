4c98560733723bcb92fdccd1080f05edecf31ade
Injure Hu <injurehu@gmail.com>
2016-04-07 00:26:16 +0800

    add cvs parser and fields

---
+++ parser/csv/MerchantId.py
@@ -0,0 +1,6 @@
+from parser.IField import IField
+
+class MerchantId(IField):
+    def __init__(self, origin_value):
+        super(MerchantId, self).__init__(origin_value)
+        self.value = MerchantId.change_format(origin_value)
